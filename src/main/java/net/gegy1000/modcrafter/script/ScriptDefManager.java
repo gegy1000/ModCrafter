5892f29bd885b2e7c4fe17ca98d42ab418175d2a
gegy1000 <gegy1000@gmail.com>
2015-10-03 16:33:34 +0200

    Temporarily remove containers

--- src/main/java/net/gegy1000/modcrafter/script/ScriptDefManager.java
+++ src/main/java/net/gegy1000/modcrafter/script/ScriptDefManager.java
@@ -5,18 +5,18 @@ import net.gegy1000.modcrafter.ModCrafterAPI;
 public class ScriptDefManager
 {
     public static final ScriptDef printConsole = new ScriptDefPrintConsole();
-    public static final ScriptDef hatTest = new ScriptDefPreInit();
-    public static final ScriptDef containerTest = new ScriptDefContainerTest();
+    public static final ScriptDef preInit = new ScriptDefPreInit();
+//    public static final ScriptDef containerTest = new ScriptDefContainerTest();
 
     public static void init()
     {
         // Hats
-        ModCrafterAPI.registerScriptDef(hatTest);
+        ModCrafterAPI.registerScriptDef(preInit);
 
         // Blocks
         ModCrafterAPI.registerScriptDef(printConsole);
 
         // Containers
-        ModCrafterAPI.registerScriptDef(containerTest);
+//        ModCrafterAPI.registerScriptDef(containerTest);
     }
 }
